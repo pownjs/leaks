67d839f2a34f9ee1dd064bc7dcccf545e76eca8b
Petko D. Petkov <pdp@websecurify.com>
2021-03-02 17:01:56 +0000

    Fix tests.

--- test/leaks.js
+++ test/leaks.js
@@ -5,21 +5,21 @@ const { compileCollection } = require('../lib/helpers')
 
 describe('LeaksPilot', () => {
     describe('#iterateOverSearch', () => {
-        it('must not produce results', () => {
+        it('must not produce results', async() => {
             const db = {}
 
             const lp = new LeaksPilot({ db })
 
             const results = []
 
-            for (const match of lp.iterateOverSearch('')) {
+            for await (const match of lp.iterateOverSearch('')) {
                 results.push(match)
             }
 
             assert.ok(results.length === 0)
         })
 
-        it('must produce results', () => {
+        it('must produce results', async() => {
             const db = {
                 test: compileCollection({
                     checks: [{
@@ -32,7 +32,7 @@ describe('LeaksPilot', () => {
 
             const results = []
 
-            for (const match of lp.iterateOverSearch('a b secret c secret x secret y z')) {
+            for await (const match of lp.iterateOverSearch('a b secret c secret x secret y z')) {
                 results.push(match)
             }
 
@@ -40,7 +40,7 @@ describe('LeaksPilot', () => {
             assert.ok(results.every((result) => result.index > 0))
         })
 
-        it('must produce results with groups', () => {
+        it('must produce results with groups', async() => {
             const db = {
                 test: compileCollection({
                     checks: [{
@@ -53,7 +53,7 @@ describe('LeaksPilot', () => {
 
             const results = []
 
-            for (const match of lp.iterateOverSearch('a b secret1 c secret2 x secret3 y z')) {
+            for await (const match of lp.iterateOverSearch('a b secret1 c secret2 x secret3 y z')) {
                 results.push(match)
             }
 
@@ -64,7 +64,7 @@ describe('LeaksPilot', () => {
     })
 
     describe('#iterateOverSearchPerCodeLine', () => {
-        it('must produce results with groups', () => {
+        it('must produce results with groups', async() => {
             const db = {
                 test: compileCollection({
                     checks: [{
@@ -77,7 +77,7 @@ describe('LeaksPilot', () => {
 
             const results = []
 
-            for (const match of lp.iterateOverSearchPerCodeLine('a\nb\nsecret1\nc\nsecret2\nx\nsecret3\ny\nz')) {
+            for await (const match of lp.iterateOverSearchPerCodeLine('a\nb\nsecret1\nc\nsecret2\nx\nsecret3\ny\nz')) {
                 results.push(match)
             }
 
@@ -86,7 +86,7 @@ describe('LeaksPilot', () => {
             assert.deepEqual(results.map((result) => result.find), ['secret1', 'secret2', 'secret3'])
         })
 
-        it('must produce results with groups', () => {
+        it('must produce results with groups', async() => {
             const db = {
                 test: compileCollection({
                     checks: [{
@@ -99,7 +99,7 @@ describe('LeaksPilot', () => {
 
             const results = []
 
-            for (const match of lp.iterateOverSearchPerCodeLine('a;b;secret1;c;secret2;x;secret3;y;z')) {
+            for await (const match of lp.iterateOverSearchPerCodeLine('a;b;secret1;c;secret2;x;secret3;y;z')) {
                 results.push(match)
             }
 
